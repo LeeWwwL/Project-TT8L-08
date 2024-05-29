699f03409a74add60c29d2edd0a3b8fb0986f72b
ZhanHan <zhanhan0@gmail.com>
2024-05-29 12:28:08 +0800

    create a link from index.html to index-login.php

--- php/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
- 
- $con = mysqli_connect("localhost","root","","php_system") or die("Couldn't connect");
-
-?>
---
+++ php_system/php/config.php
@@ -0,0 +1,3 @@
+<?php 
+ $con = mysqli_connect("localhost","root","","Project-TT8L-08") or die("Couldn't connect");
+?>
